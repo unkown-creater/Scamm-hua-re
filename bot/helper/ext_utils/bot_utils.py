d51a6f5027fe7a3941bef6cdded4909fa564253a
frgod321@gmail.com <frgod321@gmail.com>
2023-09-27 12:59:45 +0530

    hii

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -169,8 +169,7 @@ def get_readable_message():
         msg += f"💾Filename: {escape(f'{download.name()}')}\n\n"
         msg += f"<b>⚡{download.status()}...</b>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
-            msg += f"\n⚙️ <a href='https://t.me/powerleech'[{get_progress_bar_string(download.progress())} {download.progress()}]</a>"
-            msg += f"\n\n🔰 Progress: {download.processed_bytes()} of {download.size()}"
+            msg += f"\n⚙️[{get_progress_bar_string(download.progress())} {download.progress()}]"            msg += f"\n\n🔰 Progress: {download.processed_bytes()} of {download.size()}"
             msg += f"\n🔰User: {source(download)}"
             msg += f"\n🔰Speed: {download.speed()}"
             msg += f'\n🔰Estimated: {download.eta()}'

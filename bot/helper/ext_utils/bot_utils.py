974ee236dda67fa0ddfccb5432fe6e97718833a5
frgod321@gmail.com <frgod321@gmail.com>
2023-09-27 13:49:17 +0530

    bold file name

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -166,7 +166,7 @@ def get_readable_message():
         globals()['STATUS_START'] = STATUS_LIMIT * (PAGES - 1)
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
-        msg += f"💾Filename: {escape(f'{download.name()}')}\n\n"
+        msg += f"💾Filename:<b> {escape(f'{download.name()}')}</b>\n\n"
         msg += f"<b>⚡{download.status()}...</b>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n⚙️[{get_progress_bar_string(download.progress())} {download.progress()}]"
